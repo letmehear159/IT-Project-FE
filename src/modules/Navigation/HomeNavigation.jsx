f65bee9e4c2cdf5274189533c294dd8e32eb714f
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-11-17 02:45:37 +0700

    Add home navigation

---
+++ src/modules/Navigation/HomeNavigation.jsx
@@ -0,0 +1,19 @@
+import GearNavigation from "@/modules/Navigation/GearNavigation.jsx";
+
+export default function HomeNavigation() {
+    return (
+        <div>
+            <GearNavigation imgBlack='laptop-black' imgWhite='laptop-white' title='Laptop' linkHref='#'/>
+            {/*<GearNavigation imgBlack='laptop-gaming-b' imgWhite='laptop-gaming-w' title='Laptop Gaming' linkHref='#'/>*/}
+            {/*<GearNavigation imgBlack='graphic-card' imgWhite='graphic-card (1)' title='Graphic Card' linkHref='#'/>*/}
+            {/*<GearNavigation imgBlack='mother-board' imgWhite='mother-board (1)' title='Mother Board' linkHref='#'/>*/}
+            {/*<GearNavigation imgBlack='cpu' imgWhite='cpu (1)' title='CPU' linkHref='#'/>*/}
+            {/*<GearNavigation imgBlack='hosting' imgWhite='hosting (1)' title='Storage' linkHref='#'/>*/}
+            {/*<GearNavigation imgBlack='ram' imgWhite='ram (1)' title='Ram' linkHref='#'/>*/}
+            {/*<GearNavigation imgBlack='monitor' imgWhite='monitor (1)' title='Monitor' linkHref='#'/>*/}
+
+        </div>
+
+
+    );
+}
