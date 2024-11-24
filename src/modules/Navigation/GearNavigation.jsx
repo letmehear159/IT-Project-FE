90f843072c055c03b6f1b5077da5cc5425ddc030
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-11-24 23:49:26 +0700

    Update width

--- src/modules/Navigation/GearNavigation.jsx
+++ src/modules/Navigation/GearNavigation.jsx
@@ -1,15 +1,11 @@
 import {imageSrc} from "@/constants/index.ts";
 import '@/modules/module_css/navigation.css'
-import {
-    HoverCard,
-    HoverCardContent,
-    HoverCardTrigger,
-} from "@/components/ui/hover-card"
-import {Button} from "@/components/ui/button.jsx";
+import * as Tooltip from "@radix-ui/react-tooltip";
+
 import {useState} from "react";
 
 
-export default function GearNavigation({linkHref, imgBlack, imgWhite, title}) {
+export default function GearNavigation({linkHref, imgBlack, imgWhite, title, marginTop, children}) {
     const [isHovered, setHovered] = useState(false);
     const handleMouseEnter = () => {
         setHovered(true);
@@ -18,33 +14,39 @@ export default function GearNavigation({linkHref, imgBlack, imgWhite, title}) {
         setHovered(false);
     }
     return (
-        <div className='relative'
-             onMouseEnter={handleMouseEnter}
-             onMouseLeave={handleMouseLeave}
-        >
-            <HoverCard openDelay={5} closeDelay={0}>
-                <HoverCardTrigger asChild>
-                    <a className={`flex navi relative w-6/12 group my-2 ${isHovered ? 'bg-red-500' : ''}`}
-                       href={linkHref}>
-                        <img src={`${imageSrc}${isHovered ? imgWhite : imgBlack}.png`} className={'w-6'} alt={title}/>
-                        {/*<img src={`${imageSrc}${imgBlack}.png`} className='w-6 group-hover:hidden' alt={title}/>*/}
-                        {/*<img src={`${imageSrc}${imgWhite}.png`} className='w-6 hidden group-hover:block' alt={title}/>*/}
-                        <div className={`ml-1.5 text-black font-normal  ${isHovered ? 'text-white' : ''}`}>{title}</div>
-                        <img src={`${imageSrc}${isHovered ? 'right-arrow-white' : 'right-arrow-black'}.png`}
-                             className='ml-auto mr-3 w-3 h-3 mt-1.5' alt={title}/>
-                        {isHovered && (
-                            <img
-                                src={`${imageSrc}play.png`}
-                                className='w-8 h-6 absolute -right-small-navi top-0 z-20'
-                            />
-                        )}
-                    </a>
-                </HoverCardTrigger>
-                <HoverCardContent className={'w-80 absolute top-0 left-full -mt-1 ml-4'}>
-                    <div className={`w-10/12`}>This is content</div>
-                </HoverCardContent>
-            </HoverCard>
-
+        <div className={''}>
+            <Tooltip.Provider>
+                <Tooltip.Root defaultOpen={true} delayDuration={0}>
+                    <div className={'grid grid-cols-12'}>
+                        <Tooltip.Trigger asChild className={' bg-white rounded'} style={{width:'250px'}}>
+                            <div onMouseEnter={handleMouseEnter}
+                                 onMouseLeave={handleMouseLeave}>
+                                <a className={`flex navi relative  group p-2 px-3  ${isHovered ? 'bg-red-500' : ''}`}
+                                   href={linkHref}>
+                                    <img src={`${imageSrc}${isHovered ? imgWhite : imgBlack}.png`} className={'w-6'}
+                                         alt={title}/>
+                                    <div
+                                        className={`ml-1.5 text-black font-normal  ${isHovered ? 'text-white' : ''}`}>{title}</div>
+                                    <img src={`${imageSrc}${isHovered ? 'right-arrow-white' : 'right-arrow-black'}.png`}
+                                         className='ml-auto mr-3 w-3 h-3 mt-1.5' alt={title}/>
+                                    {isHovered && (
+                                        <img
+                                            src={`${imageSrc}play.png`}
+                                            className='w-10 h-10 absolute -right-9 -right-small-navi top-0 z-20'
+                                        />
+                                    )}
+                                </a>
+                            </div>
+                        </Tooltip.Trigger>
+                        <Tooltip.Content side={'right'} align={'start'} className="TooltipContent "
+                                         style={{width: '1020px',}} sideOffset={5}>
+                            <div className={`bg-white rounded`} style={{marginTop: `${marginTop}`, height: '505px'}}>
+                                {children}
+                            </div>
+                        </Tooltip.Content>
+                    </div>
+                </Tooltip.Root>
+            </Tooltip.Provider>
 
         </div>
 
