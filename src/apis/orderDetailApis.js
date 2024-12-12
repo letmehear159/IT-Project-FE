9bc6a3bbabbf4f467f68097c009b582f2da08ab4
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-12-12 22:14:27 +0700

    Update orderDetailApis.js

--- src/apis/orderDetailApis.js
+++ src/apis/orderDetailApis.js
@@ -1,8 +1,8 @@
 import {backEndUrl} from "@/constants/index.ts";
 
 
-export const orderDetailApis={
-   async getAllOrderDetailsByCart(cartId) {
+export const orderDetailApis = {
+    async getAllOrderDetailsByCart(cartId) {
         try {
             const response = await fetch(`${backEndUrl}orderDetails/cart/${cartId}`);
             return await response.json();
@@ -10,4 +10,35 @@ export const orderDetailApis={
             console.error('Error fetching orderDetail:', error);
         }
     },
+    async createOrderDetail(cartId, productId, price) {
+        try {
+            const response = await fetch(`${backEndUrl}orderDetails/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    productId: productId,
+                    cartId: cartId,
+                    price: price,
+                })
+            });
+            return response.ok;
+        } catch (error) {
+            console.error('Error during add to cart:', error);
+        }
+    },
+
+    async deleteOrderDetail(orderDetailId) {
+        try {
+            const response = await fetch(`${backEndUrl}orderDetails/${orderDetailId}`, {
+                method: 'DELETE',
+
+            });
+            return response.ok;
+        } catch (error) {
+            console.error('Error during delete order detail:', error);
+        }
+    },
+
 }
